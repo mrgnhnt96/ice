f91062811118be64bf3cecae891fb51a518af700
Morgan Hunt <mrgnhnt96@gmail.com>
2021-12-01 18:46:29 -0600

    feat: add copy with method

---
+++ packages/generator/lib/src/domain/copy_with_method.dart
@@ -0,0 +1,158 @@
+import 'package:copywith_plus/src/domain/domain.dart';
+import 'package:copywith_plus/src/util/string_buffer_ext.dart';
+
+extension on Class {
+  String get extension {
+    return 'extension ${name}X on $name';
+  }
+
+  String get copyWithSignature {
+    return '$name copyWith';
+  }
+
+  String get privateCopyWithSignature {
+    return '$name _copyWith';
+  }
+
+  String get returnPrivateCopyWith {
+    return 'return _copyWith';
+  }
+}
+
+extension on Constructor {
+  Iterable<String> parameters(int tabAmount) {
+    return _parameters(tabAmount, (p) => p.forConstructor());
+  }
+
+  Iterable<String> objectParameters(int tabAmount) {
+    return _parameters(tabAmount, (p) => p.forConstructorAsObject());
+  }
+
+  Iterable<String> arguments(int tabAmount) {
+    return _parameters(tabAmount, (p) => p.asArgument());
+  }
+
+  Iterable<String> argumentsWithDefault(int tabAmount) {
+    return _parameters(tabAmount, (p) => p.asArgumentWithDefault());
+  }
+
+  Iterable<String> _parameters(int tabAmount, String Function(Param) toString) {
+    return params.map((p) {
+      final param = '${toString(p)},\n';
+
+      return tab(param, tabAmount);
+    });
+  }
+
+  String get returnValue {
+    return 'return $displayName';
+  }
+}
+
+extension on Param {
+  String forConstructor() {
+    return '$type $name';
+  }
+
+  String forConstructorAsObject() {
+    return 'Object? $name';
+  }
+
+  String asArgument() {
+    return '$name: $name';
+  }
+
+  String asArgumentWithDefault() {
+    return '$name: $name == kCopyWithDefault ? this.$name : $name as $type';
+  }
+}
+
+/// {@template copy_with_method}
+/// Generates a copyWith method for a class.
+/// {@endtemplate}
+class CopyWithMethod {
+  /// {@macro copy_with_method}
+  const CopyWithMethod.forSubject(Class subject) : _subject = subject;
+
+  final Class _subject;
+
+  void _privateCopyWith(StringBuffer buffer, int tabAmount) {
+    final entry = _subject.entryPoint();
+
+    _copyWithMethod(
+      buffer,
+      tabAmount,
+      signature: _subject.privateCopyWithSignature,
+      parameters: entry.objectParameters,
+      arguments: entry.argumentsWithDefault,
+      returnValue: entry.returnValue,
+    );
+  }
+
+  void _publicCopyWith(StringBuffer buffer, int tabAmount) {
+    final entry = _subject.entryPoint();
+
+    _copyWithMethod(
+      buffer,
+      tabAmount,
+      signature: _subject.copyWithSignature,
+      parameters: entry.parameters,
+      arguments: entry.arguments,
+      returnValue: _subject.returnPrivateCopyWith,
+    );
+  }
+
+  void _copyWithMethod(
+    StringBuffer buffer,
+    int tabAmount, {
+    required String signature,
+    required Iterable<String> Function(int) parameters,
+    required Iterable<String> Function(int) arguments,
+    required String returnValue,
+  }) {
+    buffer
+      ..writeObject(
+        signature,
+        open: '({',
+        tab: tabAmount,
+        appendNewLine: false,
+        body: (sig, sigTab) {
+          sig.writeAll(parameters(sigTab));
+        },
+        close: '})',
+      )
+      ..writeObject(
+        '',
+        tab: tabAmount,
+        body: (body, bodyTab) {
+          body.writeObject(
+            returnValue,
+            tab: bodyTab,
+            open: '(',
+            body: (ret, retTab) {
+              ret.writeAll(arguments(retTab));
+            },
+            close: ');',
+          );
+        },
+      );
+  }
+
+  @override
+  String toString() {
+    final buffer = StringBuffer();
+
+    buffer.writeObject(
+      _subject.extension,
+      body: (ext, extTab) {
+        _publicCopyWith(buffer, extTab);
+
+        buffer.writeln();
+
+        _privateCopyWith(buffer, extTab);
+      },
+    );
+
+    return buffer.toString();
+  }
+}
