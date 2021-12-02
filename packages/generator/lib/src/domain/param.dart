66778d8106dd73332ab037ff04692442da776d79
Morgan Hunt <mrgnhnt96@gmail.com>
2021-12-01 18:45:55 -0600

    feat: add from element

--- packages/generator/lib/src/domain/param.dart
+++ packages/generator/lib/src/domain/param.dart
@@ -1,5 +1,8 @@
 // ignore_for_file: comment_references
 
+import 'package:analyzer/dart/element/element.dart';
+import 'package:analyzer/dart/element/nullability_suffix.dart';
+
 /// {@template param}
 /// A parameter of the [Class]
 /// {@endtemplate}
@@ -10,6 +13,17 @@ class Param {
     required this.type,
     this.isNullable = false,
   });
+
+  /// Retreives the [Param] from the [ParameterElement]
+  factory Param.fromElement(ParameterElement element) {
+    final param = Param(
+      name: element.displayName,
+      type: element.type.getDisplayString(withNullability: true),
+      isNullable: element.type.nullabilitySuffix == NullabilitySuffix.question,
+    );
+
+    return param;
+  }
 
   /// The name of the [Param]
   final String name;
