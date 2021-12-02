3e3a806f5efd733028e96f08497a56fba374e82a
Morgan Hunt <mrgnhnt96@gmail.com>
2021-12-01 18:46:18 -0600

    feat: add params

--- packages/generator/lib/src/domain/constructor.dart
+++ packages/generator/lib/src/domain/constructor.dart
@@ -12,11 +12,13 @@ class Constructor {
     required this.name,
     required this.displayName,
     required this.annotations,
+    required this.params,
   });
 
   /// Gets the constructor from the [ConstructorElement]
   factory Constructor.fromElement(ConstructorElement element) {
     final annotations = <Annotation>[];
+    final params = <Param>[];
 
     for (final annotation in element.metadata) {
       final annotate = Annotation.fromElement(annotation);
@@ -28,10 +30,17 @@ class Constructor {
       annotations.add(annotate);
     }
 
+    for (final param in element.parameters) {
+      final paramElement = Param.fromElement(param);
+
+      params.add(paramElement);
+    }
+
     return Constructor(
       name: element.name,
       displayName: element.displayName,
       annotations: annotations,
+      params: params,
     );
   }
 
@@ -43,4 +52,13 @@ class Constructor {
 
   /// The annotation of the [Constructor]
   final List<Annotation> annotations;
+
+  /// The params of the [Constructor]
+  final List<Param> params;
+
+  /// Whether the [Constructor] is the copyWith entry point\
+  /// determined by the [CopyWithEntryPoint] annotation
+  bool get hasEntryPointAnnotation {
+    return annotations.any((annotation) => annotation.isEntryPoint);
+  }
 }
