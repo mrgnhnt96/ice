5c916f997c5b98ed9d715dde210779e9bc2f989c
Morgan Hunt <mrgnhnt96@gmail.com>
2021-12-03 06:22:39 -0600

    refactor: remove folder

---
+++ packages/generator/test/ice_generator/basic.dart
@@ -0,0 +1,57 @@
+// ignore_for_file: unused_element, unused_field
+
+import 'package:copywith_annotation/copywith.dart';
+
+@Ice()
+class _Example {
+  const _Example({
+    this.name,
+    this.age,
+    this.isMale,
+    this.friends,
+    this.data,
+    String? fatherName,
+  }) : _father = const _Example(name: fatherName);
+
+  final String? name;
+  final int? age;
+  final bool? isMale;
+  final List<String>? friends;
+  final Map<String, dynamic>? data;
+  final _Example? _father;
+}
+
+@IceUnionBase()
+abstract class State {
+  const State();
+}
+
+@Ice.union(State)
+class _$Initial extends State {
+  const _$Initial();
+}
+
+@Ice.union(State)
+class _$Ready extends State {
+  const _$Ready(this.data);
+
+  final String data;
+}
+
+@Ice.union(State)
+class _$Error extends State {
+  const _$Error(
+    this.data, {
+    required this.message,
+    this.fix,
+  });
+
+  final String data;
+  final String message;
+  final String? fix;
+}
+
+@Ice()
+class _ModelClass {
+  const _ModelClass();
+}
