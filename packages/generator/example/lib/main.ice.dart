bf6a49a086b58ed14e1e6969d1b4151a181cb1fe
Morgan Hunt <mrgnhnt96@gmail.com>
2021-12-03 14:38:51 -0600

    refactor: update gen file

--- packages/generator/example/lib/main.ice.dart
+++ packages/generator/example/lib/main.ice.dart
@@ -13,11 +13,23 @@ extension ExampleX on Example {
     String? text,
     int? whole,
     double? decimal,
+    bool? flag,
+    DateTime? date,
+    List<String>? list,
+    Map<String, String>? map,
+    Set<String>? set,
+    Example? example,
   }) {
     return _copyWith(
       text: text,
       whole: whole,
       decimal: decimal,
+      flag: flag,
+      date: date,
+      list: list,
+      map: map,
+      set: set,
+      example: example,
     );
   }
 
@@ -25,11 +37,23 @@ extension ExampleX on Example {
     Object? text,
     Object? whole,
     Object? decimal,
+    Object? flag,
+    Object? date,
+    Object? list,
+    Object? map,
+    Object? set,
+    Object? example,
   }) {
-    return Example.fromJson(
+    return Example._(
       text: text == kCopyWithDefault ? this.text : text as String?,
       whole: whole == kCopyWithDefault ? this.whole : whole as int?,
       decimal: decimal == kCopyWithDefault ? this.decimal : decimal as double?,
+      flag: flag == kCopyWithDefault ? this.flag : flag as bool?,
+      date: date == kCopyWithDefault ? this.date : date as DateTime?,
+      list: list == kCopyWithDefault ? this.list : list as List<String>?,
+      map: map == kCopyWithDefault ? this.map : map as Map<String, String>?,
+      set: set == kCopyWithDefault ? this.set : set as Set<String>?,
+      example: example == kCopyWithDefault ? this.example : example as Example?,
     );
   }
 }
