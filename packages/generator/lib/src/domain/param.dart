d72ff738cf6d4ed05dee3371e952eac6dd765068
Morgan Hunt <mrgnhnt96@gmail.com>
2021-12-03 16:27:18 -0600

    feat: add position and requiredness props

--- packages/generator/lib/src/domain/param.dart
+++ packages/generator/lib/src/domain/param.dart
@@ -2,6 +2,7 @@
 
 import 'package:analyzer/dart/element/element.dart';
 import 'package:analyzer/dart/element/nullability_suffix.dart';
+import 'package:ice/src/domain/enums/enums.dart';
 
 /// {@template param}
 /// A parameter of the [Class]
@@ -12,17 +13,31 @@ class Param {
     required this.name,
     required this.type,
     this.isNullable = false,
+    required this.requiredness,
+    required this.positionType,
   });
 
   /// Retreives the [Param] from the [ParameterElement]
   factory Param.fromElement(ParameterElement element) {
+    final position =
+        element.isNamed ? PositionType.named : PositionType.positioned;
+    final requiredness =
+        element.isNotOptional ? Requiredness.required : Requiredness.optional;
+
     final param = Param(
       name: element.displayName,
       type: element.type.getDisplayString(withNullability: true),
       isNullable: element.type.nullabilitySuffix == NullabilitySuffix.question,
+      positionType: position,
+      requiredness: requiredness,
     );
 
     return param;
+  }
+
+  /// Retreives the [Param]s from the [ParameterElement]s
+  static List<Param> fromElements(List<ParameterElement> elements) {
+    return elements.map((e) => Param.fromElement(e)).toList();
   }
 
   /// The name of the [Param]
@@ -33,4 +48,10 @@ class Param {
 
   /// Whether the [Param] is nullable
   final bool isNullable;
+
+  /// if the param is required
+  final Requiredness requiredness;
+
+  /// the position of the param
+  final PositionType positionType;
 }
