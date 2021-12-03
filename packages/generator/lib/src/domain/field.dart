29f19b06c7b25002c995659b24e08487bf6c2d88
Morgan Hunt <mrgnhnt96@gmail.com>
2021-12-03 06:21:06 -0600

    feat: create field

---
+++ packages/generator/lib/src/domain/field.dart
@@ -0,0 +1,58 @@
+// ignore_for_file: comment_references
+
+import 'package:analyzer/dart/element/element.dart';
+import 'package:analyzer/dart/element/nullability_suffix.dart';
+import 'package:copywith_plus/src/domain/domain.dart';
+
+/// {@template field}
+/// The [Field] of a [Class]
+/// {@endtemplate}
+class Field {
+  /// {@template field}
+  const Field({
+    required this.name,
+    required this.annotations,
+    required this.type,
+    required this.isNullable,
+    required this.isPrivate,
+  });
+
+  /// retrieves the field from the [FieldElement]
+  factory Field.fromElement(FieldElement element) {
+    final annotations = Annotation.fromElements(element.metadata);
+
+    return Field(
+      annotations: annotations,
+      isNullable: element.type.nullabilitySuffix == NullabilitySuffix.question,
+      name: element.displayName,
+      type: element.type.getDisplayString(withNullability: true),
+      isPrivate: element.isPrivate,
+    );
+  }
+
+  /// retrieves the fields from the [FieldElement]s
+  static List<Field> fromElements(List<FieldElement> elements) {
+    final fields = <Field>[];
+
+    for (final field in elements) {
+      fields.add(Field.fromElement(field));
+    }
+
+    return fields;
+  }
+
+  /// the name of the field
+  final String name;
+
+  /// the annotations for the field
+  final List<Annotation> annotations;
+
+  /// the type of the field
+  final String type;
+
+  /// if the field is nullable
+  final bool isNullable;
+
+  /// if the field is private
+  final bool isPrivate;
+}
