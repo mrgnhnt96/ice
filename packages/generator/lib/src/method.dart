b64fca598b9f8d85f6e452db49e763759bd313f5
Morgan Hunt <mrgnhnt96@gmail.com>
2021-12-05 16:37:13 -0600

    refactor!: create method generator

--- packages/generator/lib/src/method.dart
+++ packages/generator/lib/src/method.dart
@@ -4,15 +4,15 @@ import 'package:analyzer/dart/element/element.dart';
 import 'package:build/build.dart';
 import 'package:ice/src/domain/domain.dart';
 import 'package:ice/src/templates/copy_with_template.dart';
-import 'package:ice_annotation/src/copywith.dart';
+import 'package:ice_annotation/src/methods/methods.dart';
 import 'package:source_gen/source_gen.dart';
 
 /// {@template copywith_generator}
 /// A [Generator] that generates a [CopyWith] class for a given [ClassElement].
 /// {@endtemplate}
-class CopyWithGenerator extends GeneratorForAnnotation<CopyWith> {
+class MethodGenerator extends GeneratorForAnnotation<Method> {
   /// {@macro copywith_generator}
-  const CopyWithGenerator() : super();
+  const MethodGenerator() : super();
 
   @override
   String generateForAnnotatedElement(
