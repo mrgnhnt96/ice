2d3177fa05a65b2e25046e7c5b79faffc9751a30
Morgan Hunt <mrgnhnt96@gmail.com>
2021-12-05 13:04:21 -0600

    style: update imports

--- packages/generator/lib/src/ice_union_base.dart
+++ packages/generator/lib/src/ice_union_base.dart
@@ -1,8 +1,10 @@
+// ignore_for_file: implementation_imports
+
 import 'package:analyzer/dart/element/element.dart';
 import 'package:build/build.dart';
 import 'package:ice/src/domain/domain.dart';
 import 'package:ice/src/templates/templates.dart';
-import 'package:ice_annotation/ice.dart';
+import 'package:ice_annotation/src/ice.dart';
 import 'package:source_gen/source_gen.dart';
 
 /// {@template ice_generator}
