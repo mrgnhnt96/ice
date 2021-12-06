a51b0abb14fbed11d03906919b64bb52ac53a979
Morgan Hunt <mrgnhnt96@gmail.com>
2021-12-05 18:46:56 -0600

    fix: change target type to class

--- packages/annotation/lib/src/methods/props.dart
+++ packages/annotation/lib/src/methods/props.dart
@@ -1,7 +1,7 @@
 import 'package:ice_annotation/src/methods/method.dart';
 import 'package:meta/meta_meta.dart';
 
-@Target({TargetKind.field})
+@Target({TargetKind.classType})
 class Props extends Method {
   const Props();
 }
