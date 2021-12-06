024bf1e5a81cc5671fd4d1e62892c0133a12e5b0
Morgan Hunt <mrgnhnt96@gmail.com>
2021-12-06 08:21:18 -0600

    feat: create props template

--- packages/generator/lib/src/templates/props_template.dart
+++ packages/generator/lib/src/templates/props_template.dart
@@ -1,15 +1,54 @@
 import 'package:ice/src/domain/domain.dart';
+import 'package:ice/src/templates/template.dart';
 import 'package:ice/src/util/string_buffer_ext.dart';
 
-class PropsTemplate {
-  const PropsTemplate.forSubject(Class subject) : _subject = subject;
+class PropsTemplate extends Template {
+  const PropsTemplate.forSubject(Class subject) : super(subject);
 
-  final Class _subject;
+  void _writeAsOverride(StringBuffer buffer) {
+    buffer
+      ..writeln('@override')
+      ..writeObject(
+        'List<Object?> get props',
+        body: () {
+          _writeReturn(buffer);
+        },
+      );
+  }
+
+  void _writeAsPrivateFunction(StringBuffer buffer) {
+    final genClassName = subject.generatedName(retainPrivate: false);
+
+    buffer.writeObject(
+      'List<Object?> _${genClassName}Props()',
+      body: () {
+        _writeReturn(buffer);
+      },
+    );
+  }
+
+  void _writeReturn(StringBuffer buffer) {
+    buffer
+      ..write('return [')
+      ..writeAll(subject.fields.map<String>((e) => e.name), ', ')
+      ..writeln('];');
+  }
 
   @override
   String toString() {
     final buffer = StringBuffer();
 
+    _writeAsOverride(buffer);
+
     return buffer.toString();
+  }
+
+  @override
+  void addToBuffer(StringBuffer buffer, {bool asOverride = true}) {
+    if (asOverride) {
+      _writeAsOverride(buffer);
+    } else {
+      _writeAsPrivateFunction(buffer);
+    }
   }
 }
