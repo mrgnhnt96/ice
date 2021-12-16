ecc10b25330956429e4b042d70cd0e07a82a6c67
Morgan Hunt <mrgnhnt96@gmail.com>
2021-12-15 20:58:07 -0600

    feat: add const to constructor

--- packages/generator/lib/ice_builder.dart
+++ packages/generator/lib/ice_builder.dart
@@ -13,7 +13,7 @@ import 'package:source_gen/source_gen.dart';
 /// {@endtemplate}
 class IceBuilder extends PackageBuilder {
   /// {@macro ice_builder}
-  IceBuilder() : super(name: 'IceGenerator', extension: '.ice.dart');
+  const IceBuilder() : super(name: 'IceGenerator', extension: '.ice.dart');
 
   @override
   List<Generator> get generators => [
