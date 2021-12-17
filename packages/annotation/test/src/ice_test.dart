3189039e4529f0949837f5fee86075fa247ca0c7
Morgan Hunt <mrgnhnt96@gmail.com>
2021-12-17 13:13:37 -0600

    feat: add tests for ice

---
+++ packages/annotation/test/src/ice_test.dart
@@ -0,0 +1,22 @@
+import 'package:ice_annotation/src/ice.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('$IceUnion', () {
+    test('converts type to string', () {
+      const union = IceUnion.of(_TestClass);
+
+      expect(union.base, '$_TestClass');
+    });
+
+    test('base is $IceUnion when #create', () {
+      const union = IceUnion.create;
+
+      expect(union.base, '$IceUnion');
+    });
+  });
+}
+
+class _TestClass {
+  const _TestClass();
+}
