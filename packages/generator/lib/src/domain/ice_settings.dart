8e7fe44ec81d1abd57bc2db5e49ff4bd44c203b0
Morgan Hunt <mrgnhnt96@gmail.com>
2021-12-17 13:26:41 -0600

    refactor: copyWithTypeSafe to CopyWithType

--- packages/generator/lib/src/domain/ice_settings.dart
+++ packages/generator/lib/src/domain/ice_settings.dart
@@ -1,3 +1,4 @@
+import 'package:ice/src/domain/enums/copy_with_type_ext.dart';
 import 'package:ice_annotation/ice.dart';
 
 /// {@template ice_settings}
@@ -7,16 +8,19 @@ class IceSettings implements Ice {
   /// {@macro ice_settings}
   const IceSettings({
     this.copyWith = true,
-    this.copyWithTypeSafe = true,
+    this.copyWithType = CopyWithType.simple,
     this.equatable = true,
     this.tostring = true,
   });
 
   /// {@macro ice_settings}
   factory IceSettings.fromOptions(Map<String, dynamic> config) {
+    const conv = CopyWithTypeConv(defaultValue: CopyWithType.simple);
+    final copyWithType = conv.fromJson(config['copy_with_type'] as String);
+
     return IceSettings(
       copyWith: config['copy_with'] as bool? ?? true,
-      copyWithTypeSafe: config['copy_with_type_safe'] as bool? ?? true,
+      copyWithType: copyWithType,
       equatable: config['equatable'] as bool? ?? true,
       tostring: config['tostring'] as bool? ?? true,
     );
@@ -26,7 +30,7 @@ class IceSettings implements Ice {
   final bool copyWith;
 
   @override
-  final bool copyWithTypeSafe;
+  final CopyWithType copyWithType;
 
   @override
   final bool equatable;
