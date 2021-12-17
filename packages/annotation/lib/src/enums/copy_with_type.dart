abf252b6ad1742f18369b78cf55d9f958cec8d93
Morgan Hunt <mrgnhnt96@gmail.com>
2021-12-17 13:10:38 -0600

    style: add docs

--- packages/annotation/lib/src/enums/copy_with_type.dart
+++ packages/annotation/lib/src/enums/copy_with_type.dart
@@ -1,4 +1,16 @@
+/// The type of copyWith method that will be generated
 enum CopyWithType {
+  /// follows convention of
+  ///
+  /// ```dart
+  /// myClass.copyWith(field: newValue);
+  /// ```
   simple,
+
+  /// follows convention of
+  ///
+  /// ```dart
+  /// myClass.copyWith(field: (currentValue) => newValue);
+  /// ```
   nullable,
 }
