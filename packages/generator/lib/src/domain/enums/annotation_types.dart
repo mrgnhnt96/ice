0cc0154f6170b2e395c9999a9f6d8de046101bcd
Morgan Hunt <mrgnhnt96@gmail.com>
2021-12-18 09:16:15 -0600

    feat: add ext to get annotations

--- packages/generator/lib/src/domain/enums/annotation_types.dart
+++ packages/generator/lib/src/domain/enums/annotation_types.dart
@@ -1,5 +1,7 @@
 // ignore_for_file: comment_references, implementation_imports
 
+import 'package:analyzer/dart/element/element.dart';
+import 'package:analyzer/src/dart/element/element.dart';
 import 'package:enum_assist_annotation/enum_assist_annotation.dart';
 import 'package:ice_annotation/src/ice.dart';
 import 'package:ice_annotation/src/methods/methods.dart';
@@ -55,6 +57,37 @@ enum AnnotationTypes {
 
   /// any other annotation
   other,
+}
+
+/// ext on [ElementAnnotation] to check for the annotation type
+extension ElementAnnotationListX on Iterable<ElementAnnotation> {
+  /// check for the annotation type
+  bool containsAnnotation(AnnotationTypes type) {
+    const conv = AnnotationTypesConv.nullable;
+
+    return any((e) {
+      final name = (e as ElementAnnotationImpl).annotationAst.name.name;
+      final result = conv.fromJson(name);
+
+      return result != null;
+    });
+  }
+}
+
+/// ext to check for the annotation type
+extension AnnotationTypesListX on Set<Object> {
+  /// gets the annotation type from the [ElementAnnotation]
+  AnnotationTypes? getAnnotationFrom(ElementAnnotation annotation) {
+    final name = (annotation as ElementAnnotationImpl).annotationAst.name.name;
+
+    if (!contains(name)) {
+      return null;
+    }
+
+    const conv = AnnotationTypesConv.nullable;
+
+    return conv.fromJson(name);
+  }
 }
 
 /// Annotations for [AnnotatinTypesX]
