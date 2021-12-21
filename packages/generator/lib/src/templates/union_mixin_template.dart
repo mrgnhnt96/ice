82ef08127eba0017515142470a706026d7588bcd
Morgan Hunt <mrgnhnt96@gmail.com>
2021-12-21 15:35:48 -0600

    feat: config union mixin

--- packages/generator/lib/src/templates/union_mixin_template.dart
+++ packages/generator/lib/src/templates/union_mixin_template.dart
@@ -27,7 +27,7 @@ extension on Class {
 
   String get nameAsArg => nonPrivateName.toCamelCase();
 
-  String resultArg({bool isRequired = false}) {
+  String mapParams({bool isRequired = false}) {
     final nullableStr = isRequired ? '' : '?';
     final keyword = isRequired ? 'required ' : '';
     return '${keyword}_Result<R, $genName>$nullableStr $nameAsArg';
@@ -88,6 +88,22 @@ extension on Constructor {
   }
 }
 
+extension on StringBuffer {
+  void writeMap(
+    Iterable<Class> classes,
+    String Function(Class) returnStr,
+  ) {
+    writeObject(
+      'return map',
+      open: '(',
+      close: ');',
+      body: () {
+        writeAll(classes.map<String>(returnStr), ',\n');
+      },
+    );
+  }
+}
+
 /// A template to generate methods for
 /// - copyWith()
 /// - Equatable Props
@@ -99,138 +115,260 @@ class UnionMixinTemplate extends Template {
   ) : super.wrapper(subject);
   final List<Class> subClasses;
 
+  String get result => '_Result';
+
   @override
   void generate(StringBuffer buffer) {
-    //   buffer
-    //     ..writeln(
-    // ignore: lines_longer_than_80_chars
-    //       'typedef _Result<R, T extends ${subject.unionName}> = R Function(T);',
-    //     )
-    //     ..writeln('typedef _NoResult<R> = R Function();');
+    _writeSupport(buffer);
+    _writeUnionBase(buffer);
+
+    buffer.writeObject(
+      'mixin _\$${subject.name}Mixin',
+      body: () {
+        _writePatternMatches(buffer);
+        _writeIsType(buffer);
+      },
+    );
+  }
+
+  void _writeIsType(StringBuffer buffer) {
+    buffer.writeAll(subClasses.map<String>((e) => e.toIsType()), '\n');
+  }
+
+  void _writeSupport(StringBuffer buffer) {
     buffer
-      ..writeObject(
-        'abstract class ${subject.unionBase}',
+      ..writeln(
+        'typedef _Result<R, T extends ${subject.unionName}> = R Function(T);',
+      )
+      ..writeln('typedef _NoResult<R> = R Function();');
+  }
+
+  void _writeUnionBase(StringBuffer buffer) {
+    buffer.writeObject(
+      'abstract class ${subject.unionBase}',
+      body: () {
+        buffer
+          ..writeln('const ${subject.unionBase}();')
+          ..writeln()
+          ..writeln('String get \$${subject.cleanName}Type;');
+      },
+    );
+  }
+
+  void _writePatternMatches(StringBuffer buffer) {
+    void writeSwitch() {
+      buffer.writeObject(
+        'switch (this.runtimeType)',
         body: () {
           buffer
-            ..writeln('const ${subject.unionBase}();')
-            ..writeln()
-            ..writeln('String get \$${subject.cleanName}Type;');
+            ..writeAll(subClasses.map<String>((e) => e.switchCase))
+            ..writeln('default:')
+            ..writeln(
+              r"throw UnsupportedError('Unsupported type: $this');",
+            );
         },
-      )
-      ..writeln('mixin _\$${subject.name}Mixin {}');
+      );
+    }
+
+    void writePattern({
+      required Pattern pattern,
+      required PatternType type,
+    }) {
+      final toWriteSwitch = type.isBase && pattern.isMap;
+
+      final params = <String>[];
+      final args = <String>[];
+
+      for (final item in subClasses) {
+        final param = pattern.param(
+          item,
+          isRequired: type.map(
+            base: true,
+            maybe: false,
+            orNull: false,
+          ),
+        );
+        params.add(param);
+
+        if (!toWriteSwitch) {
+          final arg = type.arg(pattern, item, subject.cleanName);
+          args.add(arg);
+        }
+      }
+
+      final additionalParam = type.additionalParam;
+      if (additionalParam != null) {
+        params.add(additionalParam);
+      }
+
+      buffer.writeMethod(
+        type.name(pattern),
+        params: params,
+        body: () {
+          if (toWriteSwitch) {
+            writeSwitch();
+            return;
+          }
+
+          buffer.writeObject(
+            'return map',
+            open: '(',
+            close: ');',
+            body: () {
+              buffer.writeAll(args, ',\n');
+            },
+          );
+        },
+      );
+    }
+
+    for (final type in PatternType.values) {
+      for (final pattern in Pattern.values) {
+        writePattern(
+          pattern: pattern,
+          type: type,
+        );
+      }
+    }
+  }
+}
+
+enum Pattern {
+  map,
+  when,
+}
+
+enum PatternType {
+  base,
+  maybe,
+  orNull,
+}
+
+extension on Pattern {
+  bool get isMap => this == Pattern.map;
+
+  T map<T extends Object?>({
+    required T map,
+    required T when,
+  }) {
+    switch (this) {
+      case Pattern.map:
+        return map;
+      case Pattern.when:
+        return when;
+    }
   }
 
-  String methodEntry(String methodName, {bool isNullable = false}) {
-    final nullableStr = isNullable ? '?' : '';
-    return 'R$nullableStr $methodName<R extends Object?>';
+  T? mapOrNull<T extends Object?>({
+    T? map,
+    T? when,
+  }) {
+    return this.map(
+      map: map,
+      when: when,
+    );
   }
 
-// void _writeMixin(StringBuffer buffer) {
-//   void _writePatternMatch(
-//     String name,
-//     StringBuffer buffer, {
-//     required Iterable<String> nullableParams,
-//     required Iterable<String> requiredParams,
-//     String Function(Class)? base,
-//     required String Function(Class) maybe,
-//     required String Function(Class) orNull,
-//     bool mapWithSwitch = false,
-//   }) {
-//     if (!mapWithSwitch) {
-//       assert(base != null, 'base is required when mapWithSwitch is false');
-//     }
+  String param(
+    Class sub, {
+    required bool isRequired,
+  }) {
+    return map(
+      map: sub.mapParams(isRequired: isRequired),
+      when: sub.whenParams(isRequired: isRequired),
+    );
+  }
+}
 
-//     final camelName = name.toCamelCase();
-//     final pascalName = name.toPascalCase();
+extension on PatternType {
+  bool get isBase => this == PatternType.base;
+  T map<T extends Object?>({
+    required T base,
+    required T maybe,
+    required T orNull,
+  }) {
+    switch (this) {
+      case PatternType.base:
+        return base;
+      case PatternType.maybe:
+        return maybe;
+      case PatternType.orNull:
+        return orNull;
+    }
+  }
 
-//     buffer
-//       ..writeMethod(
-//         methodEntry(camelName),
-//         params: requiredParams,
-//         body: () {
-//           if (mapWithSwitch) {
-//             buffer.writeObject(
-//               'switch (this.runtimeType)',
-//               body: () {
-//                 buffer
-//                   ..writeAll(subtypes.map<String>((e) => e.switchCase))
-//                   ..writeln('default:')
-//                   ..writeln(
-//                     r"throw UnsupportedError('Unsupported type: $this');",
-//                   );
-//               },
-//             );
-//           } else {
-//             buffer.returnMap(subtypes, base!);
-//           }
-//         },
-//       )
-//       ..writeMethod(
-//         methodEntry('maybe$pascalName'),
-//         params: {
-//           ...nullableParams,
-//           'required _NoResult<R> orElse',
-//         },
-//         body: () {
-//           buffer.returnMap(subtypes, maybe);
-//         },
-//       )
-//       ..writeMethod(
-//         methodEntry('${camelName}OrNull', isNullable: true),
-//         params: nullableParams,
-//         body: () {
-//           buffer.returnMap(subtypes, orNull);
-//         },
-//       );
-//   }
+  T? mapOrNull<T extends Object?>({
+    T? base,
+    T? maybe,
+    T? orNull,
+  }) {
+    return map(
+      base: base,
+      maybe: maybe,
+      orNull: orNull,
+    );
+  }
 
-//   final requiredParams =
-//       subtypes.map<String>((e) => e.resultArg(isRequired: true));
-//   final nullableParams = subtypes.map<String>((e) => e.resultArg());
+  String name(Pattern pattern) {
+    var isNullable = false;
 
-//   _writePatternMatch(
-//     'map',
-//     buffer,
-//     requiredParams: requiredParams,
-//     nullableParams: nullableParams,
-//     maybe: (subject) {
-//       final varName = subject.nameAsArg;
-//       return '$varName: (state) => '
-//           '$varName?.call(state) ?? orElse()';
-//     },
-//     orNull: (subject) {
-//       final varName = subject.nameAsArg;
-//       return '$varName: (state) => '
-//           '$varName?.call(state)';
-//     },
-//     mapWithSwitch: true,
-//   );
+    final pat = pattern.name;
 
-//   final requiredWhenParams =
-//       subtypes.map<String>((e) => e.whenParams(isRequired: true));
-//   final nullableWhenParams = subtypes.map<String>((e) => e.whenParams());
+    final getName = map(
+      base: pat.toCamelCase,
+      maybe: () => 'maybe${pat.toPascalCase()}',
+      orNull: () {
+        isNullable = true;
+        return '${pat.toCamelCase()}OrNull';
+      },
+    );
 
-//   _writePatternMatch(
-//     'when',
-//     buffer,
-//     requiredParams: requiredWhenParams,
-//     nullableParams: nullableWhenParams,
-//     base: (subject) {
-//       final varName = subject.nameAsArg;
-//       return '$varName: (state) => $varName(${subject.whenArgs})';
-//     },
-//     maybe: (subject) {
-//       final varName = subject.nameAsArg;
-//       return '$varName: (state) => '
-//           '$varName?.call(${subject.whenArgs}) ?? orElse()';
-//     },
-//     orNull: (subject) {
-//       final varName = subject.nameAsArg;
-//       return '$varName: (state) => $varName?.call(${subject.whenArgs})';
-//     },
-//   );
+    String methodEntry(String methodName, {bool isNullable = false}) {
+      final nullableStr = isNullable ? '?' : '';
+      return 'R$nullableStr $methodName<R extends Object?>';
+    }
 
-//   buffer.writeAll(subtypes.map<String>((e) => e.toIsType()), '\n');
-// }
+    return methodEntry(getName(), isNullable: isNullable);
+  }
 
+  String arg(Pattern pattern, Class sub, String unionName) {
+    final name = sub.nameAsArg;
+    final args = sub.whenArgs;
+    final union = unionName.toCamelCase();
+
+    final getArg = map(
+      base: pattern.map(
+        map: () {
+          throw Exception('The map must be created');
+        },
+        when: () {
+          return '$name: ($union) => $name($args)';
+        },
+      ),
+      maybe: pattern.map(
+        map: () {
+          return '$name: ($union) => $name?.call($union) ?? orElse()';
+        },
+        when: () {
+          return '$name: ($union) => $name?.call($args) ?? orElse()';
+        },
+      ),
+      orNull: pattern.map(
+        map: () {
+          return '$name: ($union) => $name?.call($union)';
+        },
+        when: () {
+          return '$name: ($union) => $name?.call($args)';
+        },
+      ),
+    );
+
+    return getArg();
+  }
+
+  String? get additionalParam {
+    return mapOrNull(
+      maybe: 'required _NoResult<R> orElse',
+    );
+  }
 }
