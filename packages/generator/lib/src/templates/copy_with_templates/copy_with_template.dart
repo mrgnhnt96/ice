bd5be02b53d511a665d4e4264a762748b1b4563c
Morgan Hunt <mrgnhnt96@gmail.com>
2021-12-28 14:27:13 -0600

    feat: add copyWithConstructor to copyWithTemplate

--- packages/generator/lib/src/templates/copy_with_templates/copy_with_template.dart
+++ packages/generator/lib/src/templates/copy_with_templates/copy_with_template.dart
@@ -16,6 +16,26 @@ extension on Class {
       settingsCallback: (settings) => settings.copyWithType,
     );
   }
+
+  /// the constructor to be used to to generate the copyWith method
+  Constructor? copyWithConstructor() {
+    if (constructors.isEmpty) return null;
+
+    final copyWithIndex =
+        constructors.indexWhere((c) => c.isCopyWithConstructor);
+
+    if (copyWithIndex != -1) {
+      return constructors[copyWithIndex];
+    }
+
+    final defaultIndex = constructors.indexWhere((c) => c.isDefault);
+
+    if (defaultIndex != -1) {
+      return constructors[defaultIndex];
+    }
+
+    return constructors.first;
+  }
 }
 
 extension on Constructor {
