be1fa77791756252f662b6bbb82270b9e7f3606b
Morgan Hunt <mrgnhnt96@gmail.com>
2021-12-30 14:32:06 -0600

    refactor: remove as annotation

--- packages/annotation/lib/src/ice_json_serializable.dart
+++ packages/annotation/lib/src/ice_json_serializable.dart
@@ -24,40 +24,4 @@ class IceJsonSerializable extends JsonSerializable {
           ignoreUnannotated: ignoreUnannotated,
           includeIfNull: includeIfNull,
         );
-
-  /// converts all values to the json_serializable annotation
-  String get asAnnotation {
-    // ignore: deprecated_member_use
-    final defaults = JsonSerializable.defaults;
-
-    final allArgs = [
-      if (createFactory ?? true) "constructor: '_'",
-      if (createToJson != null && createToJson != defaults.createToJson)
-        'createToJson: $createToJson',
-      if (createFactory != null && createFactory != defaults.createFactory)
-        'createFactory: $createFactory',
-      if (explicitToJson != null && explicitToJson != defaults.explicitToJson)
-        'explicitToJson: $explicitToJson',
-      if (fieldRename != null && fieldRename != defaults.fieldRename)
-        'fieldRename: $fieldRename',
-      if (anyMap != null && anyMap != defaults.anyMap) 'anyMap: $anyMap',
-      if (checked != null && checked != defaults.checked) 'checked: $checked',
-      if (disallowUnrecognizedKeys != null &&
-          disallowUnrecognizedKeys != defaults.disallowUnrecognizedKeys)
-        'disallowUnrecognizedKeys: $disallowUnrecognizedKeys',
-      if (ignoreUnannotated != null &&
-          ignoreUnannotated != defaults.ignoreUnannotated)
-        'ignoreUnannotated: $ignoreUnannotated',
-      if (includeIfNull != null && includeIfNull != defaults.includeIfNull)
-        'includeIfNull: $includeIfNull',
-    ];
-
-    var args = '';
-
-    if (allArgs.isNotEmpty) {
-      args += '${allArgs.join(', ')},';
-    }
-
-    return '@JsonSerializable($args)';
-  }
 }
