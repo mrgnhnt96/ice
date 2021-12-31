08b482cccd826aaa487093ecea6031889f9dc0c2
Morgan Hunt <mrgnhnt96@gmail.com>
2021-12-31 12:42:57 -0600

    feat: add debugOuput to test

--- packages/generator/test/builder/builder_test.dart
+++ packages/generator/test/builder/builder_test.dart
@@ -4,13 +4,14 @@ import 'package:build/build.dart';
 import 'package:build_test/build_test.dart';
 import 'package:generator_test/generator_test.dart';
 import 'package:ice/ice.dart';
-import 'package:ice/src/util/unified_generator.dart';
+import 'package:ice/src/domain/ice_settings.dart';
 import 'package:mocktail/mocktail.dart';
 import 'package:test/test.dart';
 
 void main() {
   setUp(() {
     const base = 'test/builder';
+    IceSettings.debugOutput = true;
     GeneratorPath.setDirPath(
       input: base,
       output: '$base/output',
