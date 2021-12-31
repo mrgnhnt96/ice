e6bd6b1e4a9574c5fd1beffedea792bf428279a2
Morgan Hunt <mrgnhnt96@gmail.com>
2021-12-31 07:23:45 -0600

    refactor: remove json_serializable and add createToJson & createFromJson

--- packages/generator/lib/src/domain/ice_settings.dart
+++ packages/generator/lib/src/domain/ice_settings.dart
@@ -14,9 +14,10 @@ class IceSettings implements Ice {
     required this.equatable,
     required this.iceToString,
     required this.ignoreGettersAsProps,
-    required this.jsonSerializable,
     required this.unionTypeKey,
     required this.formatOutput,
+    required this.createFromJson,
+    required this.createToJson,
   });
 
   /// the default settings for ice
@@ -26,9 +27,10 @@ class IceSettings implements Ice {
           equatable: true,
           iceToString: true,
           ignoreGettersAsProps: true,
-          jsonSerializable: const SettingJsonSerializable._default(),
           unionTypeKey: r'$unionType',
           formatOutput: false,
+          createToJson: true,
+          createFromJson: true,
         );
 
   /// {@macro ice_settings}
@@ -38,17 +40,19 @@ class IceSettings implements Ice {
     const conv = CopyWithTypeConv.nullable;
     final copyWithType =
         conv.fromJson(config['copy_with'] as String?) ?? defaultValues.copyWith;
-    final iceJsonSerializable = JsonSerializable.fromJson(config).toIce();
 
     return IceSettings(
       copyWith: copyWithType,
       equatable: config['equatable'] as bool? ?? defaultValues.equatable,
+      createToJson:
+          config['create_to_json'] as bool? ?? defaultValues.createToJson,
+      createFromJson:
+          config['create_from_json'] as bool? ?? defaultValues.createFromJson,
       unionTypeKey:
           config['union_type_key'] as String? ?? defaultValues.unionTypeKey,
       iceToString: config['to_string'] as bool? ?? defaultValues.iceToString,
       ignoreGettersAsProps: config['ignore_getters_as_props'] as bool? ??
           defaultValues.ignoreGettersAsProps,
-      jsonSerializable: iceJsonSerializable,
       formatOutput:
           config['format_output'] as bool? ?? defaultValues.formatOutput,
     );
@@ -86,7 +90,13 @@ class IceSettings implements Ice {
   final bool ignoreGettersAsProps;
 
   @override
-  final SettingJsonSerializable jsonSerializable;
+  IceJsonSerializable get jsonSerializable => throw UnimplementedError();
+
+  /// whether to create `toJson()`
+  final bool createToJson;
+
+  /// whether to create `fromJson()` & its constructors
+  final bool createFromJson;
 
   /// the default key to be used for union serialization
   final String unionTypeKey;
@@ -94,82 +104,4 @@ class IceSettings implements Ice {
   /// whether to format the output of the generated code
   /// using the dartfmt tool
   final bool formatOutput;
-}
-
-///
-class SettingJsonSerializable extends IceJsonSerializable {
-  ///
-  const SettingJsonSerializable({
-    required this.anyMap,
-    required this.checked,
-    required this.createFactory,
-    required this.createToJson,
-    required this.disallowUnrecognizedKeys,
-    required this.explicitToJson,
-    required this.fieldRename,
-    required this.ignoreUnannotated,
-    required this.includeIfNull,
-  });
-
-  const SettingJsonSerializable._default()
-      : this(
-          anyMap: true,
-          checked: true,
-          createFactory: true,
-          createToJson: true,
-          disallowUnrecognizedKeys: true,
-          explicitToJson: true,
-          fieldRename: FieldRename.none,
-          ignoreUnannotated: true,
-          includeIfNull: true,
-        );
-
-  @override
-  final bool anyMap;
-
-  @override
-  final bool checked;
-
-  @override
-  final bool createFactory;
-
-  @override
-  final bool createToJson;
-
-  @override
-  final bool disallowUnrecognizedKeys;
-
-  @override
-  final bool explicitToJson;
-
-  @override
-  final FieldRename fieldRename;
-
-  @override
-  final bool ignoreUnannotated;
-
-  @override
-  final bool includeIfNull;
-}
-
-///
-extension on JsonSerializable {
-  ///
-  SettingJsonSerializable toIce() {
-    // ignore: deprecated_member_use
-    const defaults = SettingJsonSerializable._default();
-
-    return SettingJsonSerializable(
-      anyMap: anyMap ?? defaults.anyMap,
-      checked: this.checked ?? defaults.checked,
-      createFactory: createFactory ?? defaults.createFactory,
-      createToJson: createToJson ?? defaults.createToJson,
-      disallowUnrecognizedKeys:
-          disallowUnrecognizedKeys ?? defaults.disallowUnrecognizedKeys,
-      explicitToJson: explicitToJson ?? defaults.explicitToJson,
-      fieldRename: fieldRename ?? defaults.fieldRename,
-      ignoreUnannotated: ignoreUnannotated ?? defaults.ignoreUnannotated,
-      includeIfNull: includeIfNull ?? defaults.includeIfNull,
-    );
-  }
 }
