fff08181552e89da9feaaa16ccf3ca77a97ab150
Morgan Hunt <mrgnhnt96@gmail.com>
2021-12-31 12:29:43 -0600

    feat: export constructor style: reorder imports

--- packages/annotation/lib/ice.dart
+++ packages/annotation/lib/ice.dart
@@ -4,7 +4,8 @@ library ice_annotation;
 
 export 'package:equatable/equatable.dart';
 
-export 'src/enums/enums.dart';
+export 'src/enums/copy_with.dart';
 export 'src/ice.dart';
+export 'src/ice_constructor.dart';
+export 'src/ice_field.dart';
 export 'src/ice_json_serializable.dart';
-export 'src/methods.dart';
