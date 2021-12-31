8883201711b43a476ea90db157c9a9940f000b7c
Morgan Hunt <mrgnhnt96@gmail.com>
2021-12-31 09:29:04 -0600

    feat: add null safe refactor: update descriptions

--- packages/annotation/lib/src/enums/copy_with.dart
+++ packages/annotation/lib/src/enums/copy_with.dart
@@ -1,20 +1,25 @@
 /// The type of copyWith method that will be generated
 enum CopyWith {
-  /// If `null` is passed within the `copyWith` method,
-  /// the current value will be returned.
+  /// NOT `null` safe
   ///
   /// ```dart
   /// myClass.copyWith(field: newValue);
   /// ```
   simple,
 
-  /// if `null` is passed within the `copyWith` method,
-  /// `null` will be returned.
+  /// 'null' safe
   ///
   /// ```dart
   /// myClass.copyWith(field: (currentValue) => newValue);
   /// ```
-  typeSafe,
+  anonymous,
+
+  /// 'null' safe
+  ///
+  /// ```dart
+  /// myClass.copyWith(field: newValue);
+  /// ```
+  nullSafe,
 
   /// does not generate a copyWith method
   none,
