841eddd1a7f208b588cd7a6e67055eaa990d398b
Morgan Hunt <mrgnhnt96@gmail.com>
2022-01-05 16:48:00 -0600

    refactor:remove tests that test output

--- packages/generator/test/builder/output/basic_generic.dart
+++
@@ -1,9 +0,0 @@
-// coverage:ignore-file
-// GENERATED CODE - DO NOT MODIFY BY HAND
-// ignore_for_file:, prefer_const_declarations, unused_local_variable
-
-part of '../basic_generic.dart';
-
-abstract class _$Example {
-  const _$Example();
-}
